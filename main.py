95ce89af6cb35a493643d9da03f49544cfbac734
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-02-10 17:17:22 -0300

    criação do campo de caracteres

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ class Calculadora(CTk):
         super().__init__()
         self.title = ("Calculadora Python")
         self.geometry = ("500x600")
+        self.create_widgets()
+    
+    def create_widgets(self):
+        #Campo de caracteres
+        self.entry = CTkEntry(self, font=('Helvetica', 20), justify=RIGHT)
+        self.entry.grid(row = 0, column = 0, columnspan = 4, sticky = NSEW)
 
 if __name__ == "__main__":
     app = Calculadora()
     app.mainloop()
-    
